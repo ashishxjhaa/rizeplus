105cf53cfbdc2b13873b1c7b503393bc7239bbe0
Ashish Jha <ashishxyzjha@gmail.com>
2025-10-04 22:04:52 +0530

    Button Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/ui/file-upload";
 
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-neutral-900 flex items-center justify-center">
+    <div className="min-h-screen w-full bg-neutral-900 flex flex-col items-center justify-center">
       <div className="w-2xl">
         <FileUpload />
+      </div>
+      <div className="mt-10">
+        <Button variant="outline">Roast Me</Button>
       </div>
     </div>
   );
