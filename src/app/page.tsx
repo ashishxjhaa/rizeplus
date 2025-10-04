143374ac8a9aec961a4b61a144ad286c267497d3
Ashish Jha <ashishxyzjha@gmail.com>
2025-10-04 21:18:02 +0530

    Upload Part Frontend Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { FileUpload } from "@/components/ui/file-upload";
 
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-black">Riz Plus</div>
+    <div className="min-h-screen w-full bg-neutral-900 flex items-center justify-center">
+      <div className="w-2xl">
+        <FileUpload />
+      </div>
+    </div>
   );
 }
